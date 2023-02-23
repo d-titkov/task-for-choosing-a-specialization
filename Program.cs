4f063845de34b3912d035116ab145b9262cc76b8
d-titkov <123071860+d-titkov@users.noreply.github.com>
2023-02-23 12:08:31 +0300

    added Program

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Enter comma-separated alphanumeric expressions:");
+string input = Console.ReadLine();
+
+string[] expressions = input.Split(',');
+string[] shortExpressions = Array.FindAll(expressions, x => x.Length < 4);
+
+string output = string.Join(", ", shortExpressions);
+output = "[" + output + "]";
+
+Console.WriteLine("Short expressions: " + output);
